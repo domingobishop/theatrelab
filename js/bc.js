3e4cf0f89fd9b35dc8e8e675c8ab1227a982bc0a
Chris Bishop <domingobishop@users.noreply.github.com>
2019-07-23 13:52:17 +0100

    cookie notice

--- js/bc.js
+++ js/bc.js
@@ -29,4 +29,46 @@ function resize()
     }
 }
 
+tSetThisCookie = function (name, days) {
+    var d = new Date();
+    d.setTime(d.getTime() + 1000 * 60 * 60 * 24 * days);
+    document.cookie = name + "=true;path=/;expires=" + d.toGMTString() + ';';
+};
+
+tCheckForThisCookie = function (name) {
+    if (document.cookie.indexOf(name) === -1) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+$(document).ready(function () {
+
+    $(function () {
+        if (!tCheckForThisCookie("dontShowCookieNotice")) {
+            $('<div class="cookieNotice">We use cookies to improve services and ensure they work for you. Read our <a title="Our cookie policy" href="https://theatrelab.co.uk/privacy-policy/">cookie policy</a>. <a title="Close cookie policy notice" href="#" id="cookieCutter">Close</a></div>').css({
+                padding: '5px',
+                "text-align": "center",
+                backgroundColor: '#FCE45C',
+                position: 'fixed',
+                bottom: 0,
+                'font-size': '14px',
+                width: '100%',
+                display: 'none'
+            }).appendTo('body');
+
+            setTimeout(function () {
+                $('.cookieNotice').slideDown(1000);
+            }, 1000);
+        }
+    });
+
+    $(document).on('click', '#cookieCutter', function (e) {
+        e.preventDefault();
+        tSetThisCookie('dontShowCookieNotice', 365);
+        $('.cookieNotice').hide();
+    });
+
+});
 
